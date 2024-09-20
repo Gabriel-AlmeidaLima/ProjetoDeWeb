0f43c699583373a19c55bbe6334f12863a7216cb
spetil <109627646+spetil@users.noreply.github.com>
2024-09-19 22:22:21 -0300

    Add files via upload

---
+++ front_end/src/components/ListaDoComentario.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>Lista comentário</div>
+    <div v-for="comentario in listaDoComentario" :key="comentario.id">
+      {{ comentario }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listaDoComentario: [],
+    };
+  },
+  mounted() {
+    this.$store.dispatch("getListaDeComentario").the((Response) => {
+      this.listaDoComentario = Response;
+    });
+  },
+};
+</script>
+
+<style></style>
